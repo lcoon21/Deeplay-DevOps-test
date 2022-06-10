da0cca056c606eb5916c568ace779f1deb5d400d
kuronaa <julia46543@gmail.com>
2022-06-10 17:20:21 +0700

    fix

--- 1st/script.sh
+++ 1st/script.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-FILENAME=$1
-IP=10.1.192.38
-
-cat "$FILENAME" | grep $IP | awk ' { match($0, /.+sid=\/(.+)\/&/, sid);} 
+cat $1 | grep 10.1.192.38 | awk ' { match($0, /.+sid=\/(.+)\/&/, sid);} 
 {print sid[1];}' | sort
